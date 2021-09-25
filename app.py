1d98834c5272471a468d0cc94ee64827a41c0d08
AlexHoop140 <alexhoop.pro@gmail.com>
2021-09-25 23:59:12 +0700

    mamamam

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from website import create_app
+from flask import Flask
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run()
+    from waitress import serve
+    serve(app, host="0.0.0.0")
+    app.debug = True
